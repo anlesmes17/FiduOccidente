65e6ba37cd6cc685de03c5fecab569d5b5877d58
anlesmes17 <98337583+anlesmes17@users.noreply.github.com>
2022-10-10 11:24:48 -0500

    Update and rename AportesPorFideicomiso to AportesPorFideicomiso.sql

--- NF/AportesPorFideicomiso.sql
+++ NF/AportesPorFideicomiso.sql
@@ -3,11 +3,11 @@ WITH
 
 ################################################ Arreglo Fechas ###################################################################
 BaseConFechaIndividuales as (
-SELECT distinct right(left(FECHA_COMP,10),4) as FechaAdj,* FROM `favorable-mix-333022.FiduOccidente.20221007_Individuales` 
+SELECT right(left(FECHA_COMP,10),4) as FechaAdj,* FROM `favorable-mix-333022.FiduOccidente.20221007_Individuales` 
 )
 
 ,FechaFinalIndividuales as(
-  Select distinct CASE
+  Select CASE
   WHEN FechaAdj="2017" OR FechaAdj="017 " Then 2017
   WHEN FechaAdj="2018" OR FechaAdj="018 " Then 2018
   WHEN FechaAdj="2019" OR FechaAdj="019 " Then 2019
@@ -19,11 +19,11 @@ SELECT distinct right(left(FECHA_COMP,10),4) as FechaAdj,* FROM `favorable-mix-3
 
 
 ,BaseConFechaMasivos as (
-SELECT distinct right(left(FECHA_ARCH,10),4) as FechaAdj,* FROM `favorable-mix-333022.FiduOccidente.20221007_Masivos` 
+SELECT right(left(FECHA_ARCH,10),4) as FechaAdj,* FROM `favorable-mix-333022.FiduOccidente.20221007_Masivos` 
 )
 
 ,FechaFinalMasivos as(
-  Select distinct CASE
+  Select CASE
   WHEN FechaAdj="2017" OR FechaAdj="017 " Then 2017
   WHEN FechaAdj="2018" OR FechaAdj="018 " Then 2018
   WHEN FechaAdj="2019" OR FechaAdj="019 " Then 2019
@@ -34,11 +34,11 @@ SELECT distinct right(left(FECHA_ARCH,10),4) as FechaAdj,* FROM `favorable-mix-3
 )
 
 ,BaseConFechaNotasBancarias as (
-SELECT distinct right(left(FEC_REGISTRO,10),4) as FechaAdj,* FROM `favorable-mix-333022.FiduOccidente.20221007_NotasBancarias` 
+SELECT right(left(FEC_REGISTRO,10),4) as FechaAdj,* FROM `favorable-mix-333022.FiduOccidente.20221007_NotasBancarias` 
 )
 
 ,FechaFinalNotasBancarias as(
-  Select distinct CASE
+  Select CASE
   WHEN FechaAdj="2017" OR FechaAdj="017 " Then 2017
   WHEN FechaAdj="2018" OR FechaAdj="018 " Then 2018
   WHEN FechaAdj="2019" OR FechaAdj="019 " Then 2019
@@ -51,20 +51,53 @@ SELECT distinct right(left(FEC_REGISTRO,10),4) as FechaAdj,* FROM `favorable-mix
 
 ################################################# Todos los Fideicomisos ##############################################################
 ,Fideicomisos as(
-SELECT distinct *
+SELECT distinct FechaAporte,Fideicomiso
 FROM
-(SELECT FechaAporte,Empresa FROM FechaFinalIndividuales
+(SELECT FechaAporte,Empresa as Fideicomiso FROM FechaFinalIndividuales
 UNION ALL
 SELECT FechaAporte,Fideicomiso FROM FechaFinalMasivos
 UNION ALL
-SELECT FechaAporte, Empresa FROM FechaFinalNotasBancarias
+SELECT FechaAporte, Empresa as Fideicomiso FROM FechaFinalNotasBancarias
 ))
 
 ################################################## Aportes por fideicomiso #############################################################
 
---,AportesIndividuales as(
-  Select distinct FechaAporte,Empresa,count(*)
+,AportesIndividuales as(
+  Select FechaAporte,Empresa,count(*) as AportIndividuales
   From FechaFinalIndividuales
   group by 1,2
   order by 3
+)
+
+,AportesMasivos as(
+  Select FechaAporte,Fideicomiso,count(*) as AportMasivos
+  From FechaFinalMasivos
+  group by 1,2
+  order by 3 
+)
+
+,AportesNotasBancarias as(
+  Select FechaAporte,Empresa,count(*) as AportNotasBancarias
+  From FechaFinalNotasBancarias
+  group by 1,2
+  order by 3 
+)
+
+,UnionIndividuales as(
+  Select f.*,AportIndividuales, 
+  From Fideicomisos f left join AportesIndividuales i
+  ON f.FechaAporte=i.FechaAporte AND Fideicomiso=Empresa
+)
+
+,UnionMasivos as(
+  Select f.*, AportMasivos
+  From UnionIndividuales f left join AportesMasivos m
+  ON f.FechaAporte=m.FechaAporte AND f.Fideicomiso=m.Fideicomiso
+)
+
+--,UnionNotasBancarias as(
+  Select f.*, AportNotasBancarias
+  From UnionMasivos f left join AportesNotasBancarias n
+  ON f.FechaAporte=n.FechaAporte AND Fideicomiso=n.Empresa
 --)
+
